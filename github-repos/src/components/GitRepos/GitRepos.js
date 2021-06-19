2f0f81ab636bc788f94c79c046e400bce7e92d37
Sean Michael Shea <seanshea1337@gmail.com>
2021-06-19 11:59:49 -0400

    function to get repo json.. useEffect to call repo json

--- github-repos/src/components/GitRepos/GitRepos.js
+++ github-repos/src/components/GitRepos/GitRepos.js
@@ -3,6 +3,17 @@ import React, { useState, useEffect } from "react";
 const GitRepos = () => {
 	const [repos, setRepos] = useState([]);
 	const url = "https://api.github.com/users/cod3rShea/repos";
+
+	const getRepos = async () => {
+		const response = await fetch(url);
+		const repos = await response.json();
+		console.log("repos", repos);
+	};
+
+	useEffect(() => {
+		getRepos();
+	}, []);
+
 	return (
 		<>
 			<h3>github Repos</h3>
