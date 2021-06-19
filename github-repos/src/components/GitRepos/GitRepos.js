7434e120d0c03d0b02c62525b66bf351e20e97b5
Sean Michael Shea <seanshea1337@gmail.com>
2021-06-19 11:51:07 -0400

    setup use state and set var my api repo url

--- github-repos/src/components/GitRepos/GitRepos.js
+++ github-repos/src/components/GitRepos/GitRepos.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
 
 const GitRepos = () => {
+	const [repos, setRepos] = useState([]);
+	const url = "https://api.github.com/users/cod3rShea/repos";
 	return (
 		<>
-			<h3>github users</h3>
+			<h3>github Repos</h3>
 		</>
 	);
 };
