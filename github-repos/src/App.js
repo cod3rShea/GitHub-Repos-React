f4bf3bab7a486075366d6964a30b46a8e6ffabb4
Sean Michael Shea <seanshea1337@gmail.com>
2021-06-19 11:46:12 -0400

    remove extra from create react app

---
+++ github-repos/src/App.js
@@ -0,0 +1,11 @@
+import GitRepos from "./components/GitRepos/GitRepos";
+
+function App() {
+	return (
+		<div className="App container-fluid">
+			<GitRepos />
+		</div>
+	);
+}
+
+export default App;
---
+++ github-repos/src/components/GitRepos/GitRepos.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+
+const GitRepos = () => {
+	return (
+		<>
+			<h3>github users</h3>
+		</>
+	);
+};
+
+export default GitRepos;
